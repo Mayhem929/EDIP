123631d3ebb745fc2df84db8c971345df1745dc9
SH02-tech <shaojiehu.hu@gmail.com>
2021-03-16 20:32:50 +0100

    Add colours.

--- NumeradorEjercicios/NumeradorEjercicios.cpp
+++ NumeradorEjercicios/NumeradorEjercicios.cpp
@@ -19,7 +19,14 @@ int main() {
 	
 	for (int i=0; i<num_ejercicios; i++) {
 		string nombre_ejercicio = "ejercicio-" + to_string(i+1) + ".tex";
+		
+		fichero << "\n\n%%%%%%%%%%%%%%%% EJERCICIO " << i+1 << " %%%%%%%%%%%%%%%%\n";
+		
 		fichero << "\t\\section{Problema " << i+1 << "}" << endl;
+		fichero << "\\color{blue}" << endl;
+		fichero << endl;
+		
+		fichero << "\\color{black}" << endl;
 		fichero << "\t\\input{" << nombre_ejercicio << "}";
 		fichero << endl << endl;
 	}
